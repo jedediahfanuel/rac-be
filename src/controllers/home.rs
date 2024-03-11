4e02cae6fdd938c5c4f88b068543a93fb4e92970
jedediahfanuel <jedediahf51@gmail.com>
2024-03-11 22:17:31 +0700

    🐛 :: add extra endpoint

--- src/controllers/home.rs
+++ src/controllers/home.rs
@@ -6,6 +6,12 @@ async fn current() -> Result<Json<HomeResponse>> {
     format::json(HomeResponse::new("loco"))
 }
 
+async fn currents() -> Result<Json<HomeResponse>> {
+    format::json(HomeResponse::new("loco-nice"))
+}
+
 pub fn routes() -> Routes {
-    Routes::new().add("/", get(current))
+    Routes::new()
+        .add("/", get(current))
+        .add("/nice", get(currents))
 }
