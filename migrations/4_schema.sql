7522ca127f08fb6f26b96f89630b2cbf815d8586
jedediahfanuel <jedediahf51@gmail.com>
2024-03-14 21:33:41 +0700

    🍪 :: migration 4th

---
+++ migrations/4_schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS registrant;
+
+CREATE TABLE registrant (
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(13) NOT NULL,
+    message TEXT,
+    photo TEXT
+);
